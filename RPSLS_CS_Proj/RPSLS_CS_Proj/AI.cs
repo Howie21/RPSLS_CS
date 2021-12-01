6b8991030fcfd562feaf96d61b9083a08a750a89
Howie21 <bryce9643@gmail.com>
2021-12-01 13:06:28 -0500

    Initial Commit

---
+++ RPSLS_CS_Proj/RPSLS_CS_Proj/AI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS_CS_Proj
+{
+    public class AI : ParentClass
+    {
+
+        public AI(string name, int score)
+        {
+            this.name = name;
+            this.score = score;
+            Console.WriteLine("AI {0} Online", name);
+        }
+        
+
+    }
+}
---
+++ RPSLS_CS_Proj/RPSLS_CS_Proj/ParentClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS_CS_Proj
+{
+    public abstract class ParentClass
+    {
+        public string name;
+        public int score;
+
+        public void ModifyScore()
+        {
+            this.score++;
+            Console.WriteLine("{0}'s score was incremented by 1", this.name);
+        }
+
+        public bool verifyInput(string userInput)
+        {
+            bool verification = false;
+
+            switch (userInput)
+            {
+                case "rock":
+                    verification = true; 
+                    break;
+                case "paper":
+                    verification = true;
+                    break;
+                case "scissors":
+                    verification= true;
+                    break;
+                case "lizard":
+                    verification = true;
+                    break;
+                case "spock":
+                    verification = true;
+                    break;
+                default:
+                    verification = false;
+                    break;
+            }
+
+            return verification;
+        }
+
+
+
+
+    }
+}
