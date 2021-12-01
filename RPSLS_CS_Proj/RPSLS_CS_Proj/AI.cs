4c89633b6a5a89b0dd3e1e668e1a4af69dcaf6ea
Howie21 <bryce9643@gmail.com>
2021-12-01 14:53:09 -0500

    Added functionality to AI to create list of gestures to choose from, and a method to randomly select one!

--- RPSLS_CS_Proj/RPSLS_CS_Proj/AI.cs
+++ RPSLS_CS_Proj/RPSLS_CS_Proj/AI.cs
@@ -8,15 +8,32 @@ namespace RPSLS_CS_Proj
 {
     public class AI : ParentClass
     {
+        public List<string> gestures;
 
         public AI(string name, int score)
         {
             this.name = name;
             this.score = score;
+            this.gestures = new List<string>();
+            createGestures();
             Console.WriteLine("AI {0} Online", name);
         }
-        
 
+        public void createGestures()
+        {
+            this.gestures.Add("rock");
+            this.gestures.Add("paper");
+            this.gestures.Add("scissors");
+            this.gestures.Add("lizard");
+            this.gestures.Add("spock");
+        }
+
+        public string randomGesture()
+        {
+            Random rnd = new Random();
+            int index = rnd.Next(5);
+            return this.gestures[index];
+        }
 
     }
 }
