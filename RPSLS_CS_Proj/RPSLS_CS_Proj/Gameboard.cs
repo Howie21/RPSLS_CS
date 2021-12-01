afded624a637882a008346adcabd83e16bed6ecd
Howie21 <bryce9643@gmail.com>
2021-12-01 14:20:12 -0500

    Addition of Logic for Gameboard to Run Game and helper methods to set the game up.

---
+++ RPSLS_CS_Proj/RPSLS_CS_Proj/Gameboard.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Convert;
+using static RPSLS_CS_Proj.Human;
+using static RPSLS_CS_Proj.AI;
+
+namespace RPSLS_CS_Proj
+{
+    public class Gameboard
+    {
+
+        public int roundCount;
+        public List<object> players;
+
+        public Gameboard(int roundCount) 
+        { 
+            this.roundCount = roundCount;
+            this.players = new List<object>();
+            Console.WriteLine("Gameboard Created");
+        }
+
+        public void runGame()
+        {
+            Console.WriteLine("Starting Game...");
+            int numberOfPlayers = decidePlayers();
+
+
+        }
+
+        public int decidePlayers()
+        {
+            Console.WriteLine("Enter Number of Human Players:");
+            string players = Console.ReadLine();
+            int numberOfPlayers = Convert.ToInt32(players);
+
+            return numberOfPlayers;
+        }
+
+        public string getPlayerName()
+        {
+            Console.WriteLine("Please Enter the Players name!:");
+            return Console.ReadLine();
+        }
+
+        public void createPlayers(int humanPlayers)
+        {
+            if(humanPlayers == 1)
+            {
+                string name = getPlayerName();
+                Human player1 = new Human(name, 0);
+                this.players.Add(player1);
+            }
+        }
+
+    }
+}
