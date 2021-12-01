8b675f1207b9606dc038b49ed24f53caa792dff5
Howie21 <bryce9643@gmail.com>
2021-12-01 14:20:53 -0500

    Moved switch case to Human to verifyInput instead of housing in AI Class

--- RPSLS_CS_Proj/RPSLS_CS_Proj/Human.cs
+++ RPSLS_CS_Proj/RPSLS_CS_Proj/Human.cs
@@ -16,7 +16,34 @@ namespace RPSLS_CS_Proj
             Console.WriteLine("{0} is created");
         }
 
+        public bool verifyInput(string userInput)
+        {
+               bool verification;
 
+               switch (userInput)
+               {
+                   case "rock":
+                       verification = true; 
+                       break;
+                   case "paper":
+                       verification = true;
+                       break;
+                   case "scissors":
+                       verification= true;
+                       break;
+                   case "lizard":
+                       verification = true;
+                       break;
+                   case "spock":
+                       verification = true;
+                       break;
+                   default:
+                       verification = false;
+                       break;
+               }
+
+               return verification;
+        }
 
     }
 }
--- RPSLS_CS_Proj/RPSLS_CS_Proj/ParentClass.cs
+++ RPSLS_CS_Proj/RPSLS_CS_Proj/ParentClass.cs
@@ -11,40 +11,13 @@ namespace RPSLS_CS_Proj
         public string name;
         public int score;
 
-        public void ModifyScore()
+        public void incrementScore()
         {
             this.score++;
             Console.WriteLine("{0}'s score was incremented by 1", this.name);
         }
 
-        public bool verifyInput(string userInput)
-        {
-            bool verification = false;
-
-            switch (userInput)
-            {
-                case "rock":
-                    verification = true; 
-                    break;
-                case "paper":
-                    verification = true;
-                    break;
-                case "scissors":
-                    verification= true;
-                    break;
-                case "lizard":
-                    verification = true;
-                    break;
-                case "spock":
-                    verification = true;
-                    break;
-                default:
-                    verification = false;
-                    break;
-            }
-
-            return verification;
-        }
+       
 
 
 
